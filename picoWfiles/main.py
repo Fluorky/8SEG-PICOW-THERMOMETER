9efe7df11e8429c0b95726e2ca537e047a2946a8
Fluorky <mrdyrt.inf@gmail.com>
2024-07-05 15:52:16 +0200

    Update tests of wifi

--- picoWfiles/main.py
+++ picoWfiles/main.py
@@ -3,5 +3,5 @@ import config
 import wifconnection
 
 if __name__ == "__main__":
-    thermometer8seg.main()
     wifconnection.connect_to_wifi(config.wifi_ssid, config.wifi_password)
+    thermometer8seg.main()
